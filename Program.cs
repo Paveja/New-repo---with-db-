10a8530ac8f6e2a4112630fcbe425a643b5dd1aa
Shakthi Priya <shakthipriya@Root-admins-MacBook-Air.local>
2025-01-31 18:20:00 +0530

    added health endpoint

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using dotnet06.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,7 +16,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpClient();
-
+builder.Services.AddHealthChecks()
+    .AddCheck("system", () => HealthCheckResult.Healthy("System is running"));
 // Retrieve individual connection variables
 var dbHost = builder.Configuration["DATABASE_URL"]
     ?? throw new InvalidOperationException("Environment variable 'DATABASE_URL' is not set.");
@@ -62,6 +65,20 @@ if (app.Environment.IsDevelopment())
 
 app.UsePathBase(baseUrl);
 app.UseRouting();
+
 // Map controllers
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+   ResponseWriter = async (context, report) =>
+   {
+       context.Response.ContentType = "application/json";
+       var response = new
+       {
+           success = true,
+           status = report.Status.ToString()
+       };
+       await context.Response.WriteAsJsonAsync(response);
+   }
+});
 app.MapControllers();
 app.Run();
